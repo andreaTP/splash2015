162f5021edf6462b4daeb383c78fd62e500d0fc9
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-10-22 11:35:32 +0200

    Added axes

--- spa/src/main/scala/eu/unicredit/benchmarks/Benchmarks.scala
+++ spa/src/main/scala/eu/unicredit/benchmarks/Benchmarks.scala
@@ -41,8 +41,10 @@ class Benchmark extends VueActor {
 
 	val vueTemplate =
 		"""
-			<svg width="500" height="400">
+			<svg width="500" height="420">
         <g transform="translate(40, 40)">
+					<line x1="{{xmin - 10}}" y1="{{ymin}}" x2="{{xmax + 10}}" y2="{{ymin}}" stroke="#cccccc" />
+					<line x1="{{xmin}}" y1="{{ymin + 10}}" x2="{{xmin}}" y2="{{ymax - 10}}" stroke="#cccccc" />
           <path d="{{area}}" fill="#7881C2" stroke="none" />
           <path d="{{line}}" fill="none" stroke="#7881C2" />
         </g>
@@ -53,16 +55,24 @@ class Benchmark extends VueActor {
 		vueBehaviour orElse {
 			case r: Result =>
         results :+= r
-        val curve = Stock[Result](
+        val stock = Stock[Result](
             data = List(results),
             xaccessor = _.param,
             yaccessor = _.time,
             width = 420,
             height = 360,
             closed = true
-          ).curves.head
+          )
+				val curve = stock.curves.head
+				val xmin = stock.xscale(0)
+				val xmax = stock.xscale(results.maxBy(_.param).param)
+				val ymin = stock.yscale(0)
+				val ymax = stock.yscale(results.maxBy(_.time).time)
         vue.$set("line", curve.line.path.print)
         vue.$set("area", curve.area.path.print)
-        println(curve)
+				vue.$set("xmin", xmin)
+				vue.$set("xmax", xmax)
+				vue.$set("ymin", ymin)
+				vue.$set("ymax", ymax)
 		}
 }
