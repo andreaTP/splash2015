486cfa299835ba69bc3c8c95b50c69cd10402fca
Andrea <andrea.peruffo1982@gmail.com>
2015-10-25 19:43:41 -0400

    raft to scalatags

--- spa/src/main/scala/eu/unicredit/benchmarks/Raft.scala
+++ spa/src/main/scala/eu/unicredit/benchmarks/Raft.scala
@@ -3,40 +3,38 @@ package eu.unicredit.benchmarks
 
 import scala.scalajs.js
 import js.Dynamic.literal
+import js.Dynamic.global
 
 import akka.actor._
 
-class RaftPage extends VueActor {
-  import TodoMsgs._
+class RaftPage extends VueScalaTagsActor {
+  import scalatags.Text.all._
 
-  val vueTemplate =
-    """
-      <div class="col-md-6">
-      <h1>
-          Raft Algorithm
-          <small>
-            <span class="glyphicon glyphicon-record"></span>
-          </small>
-      </h1>
-      <div>
-      	<div class="row">
-      		<ul class="nav nav-pills">
-        		<li v-on='click:startRaft()'><a href="#">Start</a></li>
-      		</ul>
-      	</div>
-      	<div class="row">
-       	<div id="RAFT"></div>
-       	</div>
-      </div>
-    """
+  def startRaft() = { 
+    global.startPlayground()
+    demo.Main.main()
+  }
 
-  override val vueMethods = literal(
-    startRaft = 
-    	() => { 
-    		js.Dynamic.global.startPlayground()
-    		demo.Main.main()
-    	}
-    )
+  def stTemplate =
+  	div(cls := "col-md-6")(
+  	  h1("Raft Algorithm ")(
+        small(
+          span(cls := "glyphicon glyphicon-record")
+        )
+      ),
+      div(
+        div(cls := "row")(
+          ul(cls := "nav nav-pills")(
+            li(on := {() => startRaft})(
+              a(href := "#")("Start")
+            )
+          )
+        )
+      ),
+      div(cls := "row")(
+        div(id := "RAFT")
+      )
+  	)
 
   def operational = vueBehaviour
 
